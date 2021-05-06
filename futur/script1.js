f8242f8a50e3dbe1df60e2b6242b22afde1f75fa
UCLIP <75576763+UCLIP@users.noreply.github.com>
2021-05-07 01:08:46 +0200

    Add files via upload

---
+++ futur/script1.js
@@ -0,0 +1,22 @@
+document.getElementById("perimeter").onclick = function () {
+    location.href = "../Perimeter/index.html";
+};
+document.getElementById("IA").onclick = function () {
+    location.href = "../IA/index.html";
+};
+document.getElementById("consequences").onclick = function () {
+    location.href = "../consequences/index.html";
+};
+document.getElementById("futur").onclick = function () {
+    location.href = "futur/index.html";
+};
+document.getElementById("acceuil").onclick = function () {
+    location.href = "../index1.html";
+};
+setTimeout(couleur, 4000);
+function couleur()
+{
+    color=prompt("entrez la couleur que vous voulez en anglais pour le site ");
+    document.body.style.backgroundColor =color;
+}
+
